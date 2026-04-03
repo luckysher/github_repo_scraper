9add79597390aa087bf5e6a850e8352fbd8ff5de
shersingh <luckyshera95@gmail.com>
2026-04-03 19:20:18 +0530

    initialize github repo scraper .py file

---
+++ github_repo_scraper.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+class GithubRepoScraper:
+    """
+    Github Repo Scraper class for scraping github repos from the given URL
+    """
+
+    def __init__(self):
+        pass
